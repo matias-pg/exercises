1f2448b51a1c38a79cd5dc270a17fed35edfb5cc
Matías Pizarro <matias@matiaspg.dev>
2022-06-19 22:28:01 -0400

    Add Go tests & rename folder

---
+++ Go/Testing/sqrt.go
@@ -0,0 +1,35 @@
+package main
+
+import "errors"
+
+var (
+	ErrNegSqrt    = errors.New("sqrt of negative number")
+	ErrNoSolution = errors.New("no solution found")
+)
+
+func Abs(val float64) float64 {
+	if val < 0 {
+		return -val
+	}
+	return val
+}
+
+func Sqrt(val float64) (float64, error) {
+	if val < 0.0 {
+		return 0.0, ErrNegSqrt
+	}
+
+	if val == 0.0 {
+		return 0.0, nil // shortcut
+	}
+
+	guess, epsilon := 1.0, 0.00001
+	for i := 0; i < 10000; i++ {
+		if Abs(guess*guess-val) <= epsilon {
+			return guess, nil
+		}
+		guess = (val/guess + guess) / 2.0
+	}
+
+	return 0.0, ErrNoSolution
+}
