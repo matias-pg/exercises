6cd3c6d1501e3977b1ee42824ef1a295c340f7b3
Matías Pizarro <matias@matiaspg.dev>
2022-06-27 00:53:18 -0400

    Move tests cases to a separate CSV file

--- Go/Testing/sqrt_test.go
+++ Go/Testing/sqrt_test.go
@@ -1,19 +1,18 @@
-// check out the history of this file for a raw test
+// check the history of this file for other versions of this same test
 
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
-
-func TestSimple(t *testing.T) {
-	val, err := Sqrt(2)
-	require.NoError(t, err)
-	require.InDelta(t, 1.414214, val, 0.001)
-}
 
 type testCase struct {
 	value    float64
@@ -21,17 +20,47 @@ type testCase struct {
 }
 
 func TestMany(t *testing.T) {
-	testCases := []testCase{
-		{0.0, 0.0},
-		{2.0, 1.414214},
-		{9.0, 3.0},
-	}
-
-	for _, tc := range testCases {
+	forEachCase(func(tc testCase) {
 		t.Run(fmt.Sprintf("%f", tc.value), func(t *testing.T) {
 			res, err := Sqrt(tc.value)
 			require.NoError(t, err)
 			require.InDelta(t, tc.expected, res, 0.001)
 		})
+	})
+}
+
+// Reads a CSV file containing the test cases for this function, and
+// executes a callback for each test case that was found
+func forEachCase(callback func(testCase)) {
+	// File from where we will get the test cases
+	f, err := os.Open("testdata/sqrt_cases.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+	r := csv.NewReader(f)
+
+	for {
+		// Read a line from the CSV file
+		// Note: this could've been shorter if I used ReadAll() to process all
+		// lines at once, but I wanted to try passing a function as argument
+		record, err := r.Read()
+
+		// Stop reading if we reached the end of the file
+		if err == io.EOF {
+			break
+		}
+
+		// If there's an error, log it and stop the execution
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Parse each column to a float64
+		value, _ := strconv.ParseFloat(record[0], 64)
+		expected, _ := strconv.ParseFloat(record[1], 64)
+
+		// Call the callback function with the values of the current line
+		callback(testCase{value, expected})
 	}
 }
